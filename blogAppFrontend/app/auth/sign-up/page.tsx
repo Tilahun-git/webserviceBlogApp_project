240bc1d9e322f910c30e1b6c4eb668598ac7ed8e
Tilahun-git <tilahuntareke8@gmail.com>
2025-12-24 16:36:30 +0300

    sign up , sign in pages and spinner are modified  here

--- blogAppFrontend/app/auth/sign-up/page.tsx
+++ blogAppFrontend/app/auth/sign-up/page.tsx
@@ -10,9 +10,9 @@ import { useRouter } from 'next/navigation';
 import { ChangeEvent, FormEvent, useState } from 'react';
 
 type FormDataType = {
-  firstname? : string;
+  firstname?: string;
   lastname?: string;
-  username? : string;
+  username?: string;
   email: string;
   password: string;
 };
@@ -70,102 +70,111 @@ export default function SignUpPage() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 dark:bg-gray-900 py-20 px-4">
-      <div className="flex flex-col md:flex-row max-w-4xl w-full bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
-         <div className="hidden md:flex flex-1 bg-linear-to-br from-indigo-900 via-white-700 to-gray-900 items-center justify-center text-white p-10">
-          <div className="text-center">
-            <h1 className="text-3xl font-bold mb-4">Welcome to Blop App</h1>
-            <p className="text-sm">
-              Join our community. Sign up to get started with your personalized dashboard
-              and enjoy exclusive content.
-            </p>
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 dark:bg-gray-900 py-20 px-4">
+        <div className="flex flex-col md:flex-row max-w-4xl w-full bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
+          <div className="hidden md:flex flex-1 bg-linear-to-br from-indigo-900 via-white-700 to-gray-900 items-center justify-center text-white p-10">
+            <div className="text-center">
+              <h1 className="text-3xl font-bold mb-4">Welcome to Blop App</h1>
+              <p className="text-sm">
+                Join our community. Sign up to get started with your personalized dashboard
+                and enjoy exclusive content.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex-1 p-8">
-        <h1 className="text-2xl font-bold text-center mb-6 text-gray-900 dark:text-white">
-          Create Account 
-        </h1>
+          <div className="flex-1 p-8">
+            <h1 className="text-2xl font-bold text-center mb-6 text-gray-900 dark:text-white">
+              Create Account
+            </h1>
 
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+              <div className="grid gap-2">
+                <Label htmlFor="firstname">First Name:</Label>
+                <Input
+                    id="firstname"
+                    type="text"
+                    placeholder="First Name"
+                    value={formData.firstname}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="lastname">Last Name:</Label>
+                <Input
+                    id="lastname"
+                    type="text"
+                    placeholder="Last Name"
+                    value={formData.lastname}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username:</Label>
+                <Input
+                    id="username"
+                    type="text"
+                    placeholder="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email:</Label>
+                <Input
+                    id="email"
+                    type="email"
+                    placeholder="name@gmail.com"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password:</Label>
+                <Input
+                    id="password"
+                    type="password"
+                    placeholder="********"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+              </div>
 
-          <div className='grid gap-2'>
-            <Label htmlFor="firstname">First Name:</Label>
-            <Input
-              id="firstname"
-              type="text"
-              placeholder="First Name"
-              value={formData.firstname}
-              onChange={handleChange}/>
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor="lastname">Last Name:</Label>
-            <Input
-              id="lastname"
-              type="text"
-              placeholder="Last Name"
-              value={formData.lastname}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor="username">Username:</Label>
-            <Input
-              id="username"
-              type="text"
-              placeholder="username"
-              value={formData.username}
-              onChange={handleChange}
-            />
-          </div>
+              {/*<Button type="submit" disabled={loading} className="mt-2 w-full">*/}
+              {/*  {loading ? (*/}
+              {/*      <>*/}
+              {/*        <Spinner />*/}
+              {/*        <span className="pl-3">Loading...</span>*/}
+              {/*      </>*/}
+              {/*  ) : (*/}
+              {/*      'Sign Up'*/}
+              {/*  )}*/}
+              {/*</Button>*/}
+              <Button type="submit" disabled={loading} className="mt-2 w-full">
+                {loading ? (
+                    <>
+                      <Spinner />
+                      <span className="pl-3">Loading...</span>
+                    </>
+                ) : (
+                    <span>Sign Up</span>
+                )}
+              </Button>
 
-          <div className='grid gap-2'>
-            <Label htmlFor="email">Email:</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="name@gmail.com"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </div>
+            </form>
 
-          <div className='grid gap-2'>
-            <Label htmlFor="password">Password:</Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="********"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
+            <div className="text-center text-sm mt-4">
+              <span>Already have an account? </span>
+              <Link href="/auth/sign-in" className="text-blue-500 font-medium hover:underline">
+                Sign In
+              </Link>
+            </div>
 
-          <Button type="submit" disabled={loading} className="mt-2 w-full">
-            {loading ? (
-              <>
-                <Spinner />
-                <span className="pl-3">Loading...</span>
-              </>
-            ) : (
-              'Sign Up'
+            {errorMessage && (
+                <Alert className="mt-4" color="failure">
+                  {errorMessage}
+                </Alert>
             )}
-          </Button>
-        </form>
-
-        <div className="text-center text-sm mt-4">
-          <span>Already have an account? </span>
-          <Link href="/auth/sign-in" className="text-blue-500 font-medium hover:underline">
-            Sign In
-          </Link>
-        </div>
-
-        {errorMessage && (
-          <Alert className="mt-4" color="failure">
-            {errorMessage}
-          </Alert>
-        )}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
