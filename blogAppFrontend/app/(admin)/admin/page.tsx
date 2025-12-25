1e0482bfd497d0f14dd3a7a4e5491702dd46221a
Oliad Dere <tibebudere2023@gmail.com>
2025-12-25 16:32:37 +0300

    add page of sdmin and user dashboard with middleware

---
+++ blogAppFrontend/app/(admin)/admin/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import DashSidebar from "@/components/AdminDash/DashSidebar";
+import DashPosts from "@/components/AdminDash/DashPosts";
+import DashUsers from "@/components/AdminDash/DashUsers";
+import DashComments from "@/components/AdminDash/DashComments";
+import DashboardComp from "@/components/AdminDash/DashboardComp";
+
+export default function Dashboard() {
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab") ?? "dash";
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 p-4">
+        {tab === "posts" && <DashPosts />}
+        {tab === "users" && <DashUsers />}
+        {tab === "comments" && <DashComments />}
+        {tab === "dash" && <DashboardComp />}
+      </div>
+    </div>
+  );
+}
